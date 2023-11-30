03348a80f23bb732ad51fe59cdf10d6c0d3cb147
SrDukeh <italoferreira256@gmail.com>
2023-11-30 14:02:42 -0300

    começo da prova

---
+++ algoritimos_e_variaveis/app.py
@@ -0,0 +1,20 @@
+nome = str(input('Qual o seu nome?'))
+idade = int(input('Qual a sua idade (somente numeros inteiros)'))
+preco = float(input('Qual o preço do Item?'))
+
+def qual_escolha():
+    while True:
+        escolha = input("Escolha entre 'True' ou 'False': ").capitalize()
+        if escolha in ['True', 'False']:
+            return escolha == 'True'
+        else:
+            print("Escolha inválida. Por favor, digite 'True' ou 'False'.")
+
+cliente_fidelidade = qual_escolha()
+
+idade_str = str(idade)
+preco = float(preco.replace(',', '.'))
+preco_str = str(preco)
+cliente_fidelidade_str = str(cliente_fidelidade)
+
+print ('Bem vindo(a) '+nome+'. Vi que sua idade é de '+idade_str+', certo?! Sendo assim, o preço realmente vai sair a '+preco_str+'! E aproveitando, gostaria de informar que sua fidadelidade esta atuando como: '+cliente_fidelidade_str+'. Espero que tenha gostado de sua consulta. Tenha um otimo dia!')
