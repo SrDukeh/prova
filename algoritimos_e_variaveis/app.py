676e3c5d6d80e822e000036b40fab0482238e0e2
SrDukeh <italoferreira256@gmail.com>
2023-11-30 15:18:29 -0300

    prova finalizada!

--- algoritimos_e_variaveis/app.py
+++ algoritimos_e_variaveis/app.py
@@ -1,22 +1,20 @@
-nome = str(input('Qual o seu nome?'))
-idade = int(input('Qual a sua idade (somente numeros inteiros)'))
-preco = float(input('Qual o preço do Item?'))
+nome = str(input('Qual o seu nome?' ))
+idade = int(input('Qual a sua idade (somente números inteiros)? '))
 
 def qual_escolha():
     while True:
-        escolha = input("Escolha entre 'True' ou 'False': ").capitalize()
-        if escolha in ['True', 'False']:
-            return escolha == 'True'
+        escolha = input("Você tem a FIDELIDADE? 'SIM' ou 'NÃO': ").upper()
+        if escolha in ['SIM', 'NÃO']:
+            return escolha == 'SIM'
         else:
-            print("Escolha inválida. Por favor, digite 'True' ou 'False'.")
+            print("Escolha inválida. Por favor, digite 'SIM' ou 'NÃO'.")
 
 cliente_fidelidade = qual_escolha()
-
-idade_str = str(idade)
 preco_str = input('Qual o preço do Item?')
-preco = float(preco_str.replace(',', '.'))
+preco_str = preco_str.replace(',', '.')
+preco = float(preco_str)
+idade_str = str(idade)
 preco_str = str(preco)
-cliente_fidelidade_str = str(cliente_fidelidade)
+status_fidelidade = 'ATIVO' if cliente_fidelidade else 'INATIVO'
 
-
-print ('Bem vindo(a) '+nome+'. Vi que sua idade é de '+idade_str+', certo?! Sendo assim, o preço realmente vai sair a '+preco_str+'! E aproveitando, gostaria de informar que sua fidadelidade esta atuando como: '+cliente_fidelidade_str+'. Espero que tenha gostado de sua consulta. Tenha um otimo dia!')
+print('Bem-vindo(a) ' + nome + '. Vi que sua idade é de ' + idade_str + ', certo?! Sendo assim, o preço realmente vai sair a ' + preco_str + '! E aproveitando, gostaria de informar que sua fidelidade está ' + status_fidelidade + '. Espero que tenha gostado de sua consulta. Tenha um ótimo dia!')
